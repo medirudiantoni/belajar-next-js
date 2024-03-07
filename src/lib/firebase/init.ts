7f501ae130aa3a24b6552a7236c047f18de8847a
medi_rudiant <medirudiantoni2@gmail.com>
2024-03-07 17:40:36 +0700

    set init firebase

--- src/lib/firebase/init.ts
+++ src/lib/firebase/init.ts
@@ -5,12 +5,18 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID
+  // apiKey: process.env.FIREBASE_API_KEY,
+  // authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  // projectId: process.env.FIREBASE_PROJECT_ID,
+  // storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  // messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  // appId: process.env.FIREBASE_APP_ID
+  apiKey: "AIzaSyD8-L3QnqXcX7-XhiUVIpx8jh0EyNM6kDE",
+  authDomain: "next-app-1-e16c0.firebaseapp.com",
+  projectId: "next-app-1-e16c0",
+  storageBucket: "next-app-1-e16c0.appspot.com",
+  messagingSenderId: "92428317834",
+  appId: "1:92428317834:web:28c14c3899fa0cbebee973"
 };
 
 // Initialize Firebase
